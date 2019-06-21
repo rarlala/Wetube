fe3e3b6ea683b6b504950632a45af3d6dff51f11
username <rarlala2015@naver.com>
2019-06-21 22:19:01 +0900

    오류해결

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //const express = require('express')
 import express from "express";
-import logger from "morgan";
+import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
@@ -9,26 +9,18 @@ const app = express();
 const PORT = 4000;
 
 const handleListening = () => 
-console.log(`Listening on : http://localhost:${PORT}`);
+console.log(`Listening on : https://localhost:${PORT}`);
 
 const handleHome = (req, res) =>  res.send('Hello from Home');
-
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-//const betweenHome = (req, res, next) => {
-//    console.log("Between");
-//    next();
-//}
-app.use(cookieParser(""));
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(helmet(""));
-app.use(morgan("tiny"));
-
-//app.use(betweenHome);
+app.use(helmet());
+app.use(morgan('combined'));
 
 app.get("/", handleHome);
-
 app.get("/profile",handleProfile);
 
 app.listen(PORT, handleListening);
