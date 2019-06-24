b81df73f9d544d5a7f491582d4c865171a941904
username <rarlala2015@naver.com>
2019-06-25 03:50:03 +0900

    PUG 학습 및 router 연동

--- app.js
+++ app.js
@@ -4,21 +4,24 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 const app = express();
 
+app.use(helmet());
+app.set('view engine', "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(helmet());
 app.use(morgan("dev"));
 
-app.use(routes.home,globalRouter);
+app.use(localMiddleware);
+
+app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
-
 
 export default app;
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,2 +1,7 @@
-export const home = (req,res) => res.send("Home");
-export const search = (req,res) => res.send("Search");
+export const home = (req,res) => res.render("home", { pageTitle : 'Home'});
+export const search = (req,res) => res.send("Search", { pageTitle : 'Search'});
+export const videos = (req,res) => res.send("Video");
+export const upload = (req,res) => res.send("Upload");
+export const videoDetail = (req,res) => res.send("Video Detail", {pageTitle: "Video Detail"});
+export const editVideo = (req,res) => res.send("Edit Video", {pageTitle: "Edit Video"});
+export const deleteVideo = (req,res) => res.send("Delete Video", {pageTitle: "Delete Video"});
